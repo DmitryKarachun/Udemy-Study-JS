0738aa823858d09dcc28ba55dc499d089a32a207
Karachun Dmitry <zmicer88@gmail.com>
2022-11-08 17:25:36 +0300

    отправка данных на сервер

--- js/kurswork.js
+++ js/kurswork.js
@@ -19,3 +19,7 @@ let timer = setInterval(function() {
 function draw(timePassed) {
   train.style.left = timePassed / 100 + 'px';
 }
+function setOptions(height, width, ...additional) {
+  console.log(height, width, additional)
+}
+setOptions(400, 500, 'red', 'top');
